62dfe0edb10b62e41c42ebe164c1bf4bb5eae009
Tayte Stokes <taytestokes9328@gmail.com>
2019-06-13 23:23:09 -0600

    can now create a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,25 @@ class PostsController < ApplicationController
 
     def new
     end
+
+    def create
+        # get the new post
+        @post = Post.new(post_params)
+        # save the post to the db
+        @post.save
+        # then redirect to a page
+        redirect_to @post
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+
+    private
+    
+    def post_params
+        # permit the title and body to come as params in the req
+        params.require(:post).permit(:title, :body)
+    end
 end
